972300959933c152a7424ef4dd7cc96c41ef68e6
Akn <sibata486@gmail.com>
2024-10-13 23:18:22 +0900

    add feat / カレントディレクトリを設定する処理を実装。

--- SoundChecker/ViewModels/MainWindowViewModel.cs
+++ SoundChecker/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using System.IO.Abstractions;
+using Prism.Mvvm;
 using SoundChecker.Models;
 
 namespace SoundChecker.ViewModels
@@ -6,6 +7,25 @@ namespace SoundChecker.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
+        private string currentDirectoryPath;
+
         public TextWrapper TextWrapper { get; } = new ();
+
+        public IDirectoryInfo CurrenDirectoryInfo { get; set; }
+
+        public string CurrentDirectoryPath
+        {
+            get => currentDirectoryPath;
+            set
+            {
+                SetProperty(ref currentDirectoryPath, value);
+                if (string.IsNullOrWhiteSpace(currentDirectoryPath))
+                {
+                    return;
+                }
+
+                CurrenDirectoryInfo = new FileSystem().DirectoryInfo.New(currentDirectoryPath);
+            }
+        }
     }
 }
